c1e8d382cee71749a4ac94b3f6196fc316b9ae4e
Vinícius Neto <viniciusbsneto@gmail.com>
2020-09-29 17:22:17 -0300

    feat: add routes

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
-function App() {
+import Routes from './routes';
+
+const App: React.FC = () => {
   return (
-    <h1>URL Manager</h1>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
