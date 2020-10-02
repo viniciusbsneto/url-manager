8cce0cb9c70c108bfab5b412eda843ed936816fb
Vinícius Neto <viniciusbsneto@gmail.com>
2020-10-02 15:56:32 -0300

    feat: add dashboard page

--- web/src/pages/Dashboard/index.tsx
+++ web/src/pages/Dashboard/index.tsx
@@ -1,9 +1,89 @@
 import React from 'react';
+import { FiExternalLink } from 'react-icons/fi';
 
-// import { Container } from './styles';
+import { Title, Form, URLList } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <h1>Dashboard</h1>;
+  return (
+    <>
+      <Title>URL Manager</Title>
+
+      <Form>
+        <input placeholder="Buscar URL" />
+        <button type="button">Pesquisar</button>
+      </Form>
+
+      <URLList>
+        <a
+          href="https://github.com/viniciusbsneto"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src="https://camo.githubusercontent.com/a49ca127dc3de0c1adbb60a2f8535f631b645c1c/68747470733a2f2f6c68332e676f6f676c6575736572636f6e74656e742e636f6d2f616d686f503679516970444f624d6d6344537a44496d65694e68487155777339624f6a367a436c424a4c76506a4750364f306952726e616a326265643958416e6777"
+            alt="URL"
+          />
+          <div>
+            <strong>github.com/viniciusbsneto</strong>
+            <p>Descrição da URL</p>
+          </div>
+
+          <FiExternalLink size={20} />
+        </a>
+
+        <a
+          href="https://github.com/viniciusbsneto"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src="https://camo.githubusercontent.com/a49ca127dc3de0c1adbb60a2f8535f631b645c1c/68747470733a2f2f6c68332e676f6f676c6575736572636f6e74656e742e636f6d2f616d686f503679516970444f624d6d6344537a44496d65694e68487155777339624f6a367a436c424a4c76506a4750364f306952726e616a326265643958416e6777"
+            alt="URL"
+          />
+          <div>
+            <strong>github.com/viniciusbsneto</strong>
+            <p>Descrição da URL</p>
+          </div>
+
+          <FiExternalLink size={20} />
+        </a>
+
+        <a
+          href="https://github.com/viniciusbsneto"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src="https://camo.githubusercontent.com/a49ca127dc3de0c1adbb60a2f8535f631b645c1c/68747470733a2f2f6c68332e676f6f676c6575736572636f6e74656e742e636f6d2f616d686f503679516970444f624d6d6344537a44496d65694e68487155777339624f6a367a436c424a4c76506a4750364f306952726e616a326265643958416e6777"
+            alt="URL"
+          />
+          <div>
+            <strong>github.com/viniciusbsneto</strong>
+            <p>Descrição da URL</p>
+          </div>
+
+          <FiExternalLink size={20} />
+        </a>
+
+        <a
+          href="https://github.com/viniciusbsneto"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src="https://camo.githubusercontent.com/a49ca127dc3de0c1adbb60a2f8535f631b645c1c/68747470733a2f2f6c68332e676f6f676c6575736572636f6e74656e742e636f6d2f616d686f503679516970444f624d6d6344537a44496d65694e68487155777339624f6a367a436c424a4c76506a4750364f306952726e616a326265643958416e6777"
+            alt="URL"
+          />
+          <div>
+            <strong>github.com/viniciusbsneto</strong>
+            <p>Descrição da URL</p>
+          </div>
+
+          <FiExternalLink size={20} />
+        </a>
+      </URLList>
+    </>
+  );
 };
 
 export default Dashboard;
---
+++ web/src/pages/Dashboard/styles.ts
@@ -0,0 +1,99 @@
+import styled from 'styled-components';
+import { shade } from 'polished';
+
+export const Container = styled.div``;
+
+export const Title = styled.h1`
+  font-size: 48px;
+  color: #212931;
+  max-width: 450px;
+  line-height: 48px;
+
+  margin-top: 80px;
+`;
+
+export const Form = styled.form`
+  margin-top: 40px;
+  max-width: 700px;
+
+  display: flex;
+
+  input {
+    flex: 1;
+    height: 70px;
+    padding: 0 24px;
+    border: 0;
+    border-radius: 5px 0 0 5px;
+
+    &::placeholder {
+      color: #a8a8b3;
+    }
+  }
+
+  button {
+    width: 210px;
+    height: 70px;
+    background: #0976cb;
+    border-radius: 0 5px 5px 0;
+    border: 0;
+    color: #fff;
+    font-weight: bold;
+    transition: background-color 0.2s;
+
+    &:hover {
+      background: ${shade(0.2, '#0976cb')};
+    }
+  }
+`;
+
+export const URLList = styled.div`
+  margin-top: 80px;
+  max-width: 700px;
+
+  a {
+    background: #fff;
+    border-radius: 5px;
+    width: 100%;
+    padding: 24px;
+    display: block;
+    text-decoration: none;
+
+    display: flex;
+    align-items: center;
+    transition: transform 0.2s;
+
+    &:hover {
+      transform: translateX(10px);
+    }
+
+    & + a {
+      margin-top: 16px;
+    }
+
+    img {
+      width: 64px;
+      height: 64px;
+      border-radius: 50%;
+    }
+
+    div {
+      margin-left: 16px;
+
+      strong {
+        font-size: 20px;
+        color: #3d3d4d;
+      }
+
+      p {
+        font-size: 18px;
+        color: #a8a8b3;
+        margin-top: 4px;
+      }
+    }
+
+    svg {
+      margin-left: auto;
+      color: #cbcbd6;
+    }
+  }
+`;
