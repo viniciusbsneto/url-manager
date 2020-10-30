55f59b50cca922e09944cf7c05b304ee90fd74ba
Vinícius Neto <viniciusbsneto@gmail.com>
2020-10-30 14:42:58 -0300

    feat: Add URLs memory storage

--- api/src/routes/urls.routes.ts
+++ api/src/routes/urls.routes.ts
@@ -1,5 +1,13 @@
 import { Router } from 'express';
 
+interface URL {
+  id: string;
+  address: string;
+  description: string;
+}
+
 const urlsRouter = Router();
+
+const urls: URL[] = [];
 
 export default urlsRouter;
