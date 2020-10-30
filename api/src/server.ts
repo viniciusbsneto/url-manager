34367c03adfac5e1dad67c36e558efde283fc823
Vinícius Neto <viniciusbsneto@gmail.com>
2020-10-30 14:23:37 -0300

    feat: Setup routes module

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,10 +1,10 @@
 import express from 'express';
 
+import routes from './routes';
+
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'URL Manager'});
-});
+app.get('/', (request, response) => response.json({ message: 'URL Manager' }));
 
 app.listen(3333, () => {
   console.log('Server started on port 3333!');
