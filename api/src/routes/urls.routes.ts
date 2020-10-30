600ab1e5e6f60f4160aa65c7767af3e8c28bb4bd
Vinícius Neto <viniciusbsneto@gmail.com>
2020-10-30 14:43:41 -0300

    feat: Add create URL route

--- api/src/routes/urls.routes.ts
+++ api/src/routes/urls.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { uuid } from 'uuidv4';
 
 interface URL {
   id: string;
@@ -9,5 +10,19 @@ interface URL {
 const urlsRouter = Router();
 
 const urls: URL[] = [];
+
+urlsRouter.post('/', (request, response) => {
+  const { address, description } = request.body;
+
+  const url = {
+    id: uuid(),
+    address,
+    description,
+  };
+
+  urls.push(url);
+
+  return response.json(url);
+});
 
 export default urlsRouter;
