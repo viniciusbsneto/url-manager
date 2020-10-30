e4f031677f6c363ed0d820cbf1fbc7b126455a54
Vinícius Neto <viniciusbsneto@gmail.com>
2020-10-30 14:32:14 -0300

    feat: Setup URLs routes

--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -1,5 +1,8 @@
 import { Router } from 'express';
+import urlsRouter from './urls.routes';
 
 const routes = Router();
+
+routes.use('/urls', urlsRouter);
 
 export default routes;
---
+++ api/src/routes/urls.routes.ts
@@ -0,0 +1,5 @@
+import { Router } from 'express';
+
+const urlsRouter = Router();
+
+export default urlsRouter;
