f3caba40b21cd738dccd7837cef47fc5ccf910b0
Vinícius Neto <viniciusbsneto@gmail.com>
2020-11-13 13:30:53 -0300

    chore: Add Express JSON parser middleware

--- api/src/server.ts
+++ api/src/server.ts
@@ -4,7 +4,8 @@ import routes from './routes';
 
 const app = express();
 
-app.get('/', (request, response) => response.json({ message: 'URL Manager' }));
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333, () => {
   console.log('Server started on port 3333!');
