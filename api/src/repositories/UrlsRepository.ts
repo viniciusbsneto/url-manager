4a7273b0c08d577153818dd78081f9edcd047411
Vinícius Neto <viniciusbsneto@gmail.com>
2020-11-17 12:26:05 -0300

    feat: Add URL repository create method

--- api/src/repositories/UrlsRepository.ts
+++ api/src/repositories/UrlsRepository.ts
@@ -6,6 +6,20 @@ class UrlsRepository {
   constructor() {
     this.urls = [];
   }
+
+  public create(address: string, description: string): URL | null {
+    const urlFound = this.urls.find(url => url.address === address);
+
+    if (urlFound) {
+      return null;
+    }
+
+    const url = new URL(address, description);
+
+    this.urls.push(url);
+
+    return url;
+  }
 }
 
 export default UrlsRepository;
