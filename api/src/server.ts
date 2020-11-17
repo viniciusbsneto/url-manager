e7b7e1d8711b2848c2653afd413412880e4776a7
Vinícius Neto <viniciusbsneto@gmail.com>
2020-11-16 23:14:57 -0300

    chore: Setup app port from .env file

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,3 +1,5 @@
+import 'dotenv/config';
+
 import express from 'express';
 import cors from 'cors';
 
@@ -9,6 +11,6 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333, () => {
+app.listen(process.env.APP_PORT, () => {
   console.log('Server started on port 3333!');
 });
