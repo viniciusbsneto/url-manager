41d71a77e2813a13a89cf8f96b2c902bcdf169b3
Vinícius Neto <viniciusbsneto@gmail.com>
2020-11-16 23:20:56 -0300

    chore: Set default app port

--- api/src/server.ts
+++ api/src/server.ts
@@ -11,6 +11,8 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(process.env.APP_PORT, () => {
+const port = process.env.APP_PORT || 3333;
+
+app.listen(port, () => {
   console.log('Server started on port 3333!');
 });
