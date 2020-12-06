9da08907a28decb8d39f1f9672aebeeec9583a5a
Vinícius Neto <viniciusbsneto@gmail.com>
2020-12-06 20:16:35 -0300

    feat: Add update URL service

---
+++ api/src/services/UpdateUrlService.ts
@@ -0,0 +1,27 @@
+import URL from '../models/URL';
+import UrlsRepository from '../repositories/UrlsRepository';
+
+interface UpdateUrlDTO {
+  address: string;
+  description: string;
+}
+
+class UpdateUrlService {
+  constructor(private readonly urlsRepository: UrlsRepository) {
+    this.urlsRepository = urlsRepository;
+  }
+
+  public execute(id: string, { address, description }: UpdateUrlDTO): URL {
+    const urlFound = this.urlsRepository.findById(id);
+
+    if (!urlFound) {
+      throw new Error('URL not found.');
+    }
+
+    const updatedUrl = this.urlsRepository.update(id, { address, description });
+
+    return updatedUrl;
+  }
+}
+
+export default UpdateUrlService;
