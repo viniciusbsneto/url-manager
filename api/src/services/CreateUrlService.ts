30fc54bf404569d27833cea42be8cfbf567cd84d
Vinícius Neto <viniciusbsneto@gmail.com>
2020-12-06 17:59:34 -0300

    feat: Add create URL service

---
+++ api/src/services/CreateUrlService.ts
@@ -0,0 +1,27 @@
+import URL from '../models/URL';
+import UrlsRepository from '../repositories/UrlsRepository';
+
+interface CreateUrlDTO {
+  address: string;
+  description: string;
+}
+
+class CreateUrlService {
+  constructor(private readonly urlsRepository: UrlsRepository) {
+    this.urlsRepository = urlsRepository;
+  }
+
+  public execute({ address, description }: CreateUrlDTO): URL {
+    const urlFound = this.urlsRepository.findByAddress(address);
+
+    if (urlFound) {
+      throw Error('URL already exists.');
+    }
+
+    const url = this.urlsRepository.create({ address, description });
+
+    return url;
+  }
+}
+
+export default CreateUrlService;
