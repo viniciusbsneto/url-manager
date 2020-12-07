78e1a3773b4cb8b699f180c9cfb89c6522791a80
Vinícius Neto <viniciusbsneto@gmail.com>
2020-12-06 21:03:35 -0300

    feat: Add create user service

---
+++ api/src/services/CreateUserService.ts
@@ -0,0 +1,28 @@
+import User from '../models/User';
+import UsersRepository from '../repositories/UsersRepository';
+
+interface CreateUserDTO {
+  name: string;
+  email: string;
+  password: string;
+}
+
+class CreateUserService {
+  constructor(private readonly usersRepository: UsersRepository) {
+    this.usersRepository = usersRepository;
+  }
+
+  public execute({ name, email, password }: CreateUserDTO): User {
+    const userFound = this.usersRepository.findByEmail(email);
+
+    if (userFound) {
+      throw Error('User already exists.');
+    }
+
+    const user = this.usersRepository.create({ name, email, password });
+
+    return user;
+  }
+}
+
+export default CreateUserService;
