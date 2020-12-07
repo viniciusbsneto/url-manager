2019cc86f3ba08991cb17eb97b38154687eda56a
Vinícius Neto <viniciusbsneto@gmail.com>
2020-12-06 21:22:37 -0300

    chore: Import users router

--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
+import usersRouter from './users.routes';
 import urlsRouter from './urls.routes';
 
 const routes = Router();
 
+routes.use('/users', usersRouter);
 routes.use('/urls', urlsRouter);
 
 export default routes;
