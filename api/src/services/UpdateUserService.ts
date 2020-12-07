b3041479fdf91583b9172e331f418e21a6ca072c
Vinícius Neto <viniciusbsneto@gmail.com>
2020-12-06 21:03:56 -0300

    feat: Add update user service

---
+++ api/src/services/UpdateUserService.ts
@@ -0,0 +1,30 @@
+import User from '../models/User';
+import UsersRepository from '../repositories/UsersRepository';
+
+interface UpdateUserDTO {
+  name: string;
+  email: string;
+}
+
+class UpdateUserService {
+  constructor(private readonly usersRepository: UsersRepository) {
+    this.usersRepository = usersRepository;
+  }
+
+  public execute(id: string, { name, email }: UpdateUserDTO): User {
+    const userFound = this.usersRepository.findById(id);
+
+    if (!userFound) {
+      throw new Error('User not found.');
+    }
+
+    const updatedUser = this.usersRepository.update(id, {
+      name,
+      email,
+    });
+
+    return updatedUser;
+  }
+}
+
+export default UpdateUserService;
